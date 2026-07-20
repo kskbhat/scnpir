0e3955ed29b14daf7510aa904bbbdb66649d3324
Shrikrishna Bhat <sankhyashodh@gmail.com>
2026-07-20 21:14:32 +0530

    Upgrade R-CMD-check to multi-OS matrix and add automated learnr tutorial rendering test

--- tests/testthat/test-tutorials.R
+++ tests/testthat/test-tutorials.R
@@ -22,3 +22,31 @@ test_that("tutorials can be discovered by learnr", {
   expect_true("name" %in% names(avail))
   expect_gte(nrow(avail), 5)
 })
+
+test_that("all 5 learnr tutorials pre-render and validate cleanly without errors", {
+  tutorials <- c(
+    "unit1_r_basics",
+    "unit2_descriptive_stats",
+    "unit3_confidence_intervals",
+    "unit4_contingency_tables",
+    "unit5_nonparametric_tests"
+  )
+
+  for (tut in tutorials) {
+    rmd_file <- system.file("tutorials", tut, paste0(tut, ".Rmd"), package = "scnpir")
+    expect_true(file.exists(rmd_file))
+
+    out_file <- tempfile(fileext = ".html")
+    on.exit(unlink(out_file), add = TRUE)
+
+    # Test knitting/pre-rendering the learnr tutorial Rmd
+    expect_no_error({
+      rmarkdown::render(
+        input = rmd_file,
+        output_file = out_file,
+        quiet = TRUE,
+        run_pandoc = FALSE
+      )
+    })
+  }
+})
